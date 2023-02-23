37ca93b78324a9f38a9d16df2add5df62be1698b
ikrom97 <ikromr04@gmail.com>
2023-02-23 11:38:43 +0500

    devlopment => marketing

--- resources/views/components/contacts-list.blade.php
+++ resources/views/components/contacts-list.blade.php
@@ -26,7 +26,7 @@
   <li class="contacts-list__item">
     <h3 class="contacts-list__title">Инвестиции и развитие</h3>
     <a class="contacts-list__phone" href="tel:+992 (44) 600 00 43">+992 (44) 600 00 43</a>
-    <a class="contacts-list__email" href="mailto:development@koinotinav.tj">development@koinotinav.tj</a>
+    <a class="contacts-list__email" href="mailto:marketing@koinotinav.tj">marketing@koinotinav.tj</a>
     <span class="contacts-list__icon">
       <svg width="16" height="17">
         <use xlink:href="#search"></use>
